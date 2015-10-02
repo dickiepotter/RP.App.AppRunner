59ee894ee03ff042444bf366f1a577c348301164
Richard Potter <dickiepotter@hotmail.com>
2015-10-02 18:38:48 +0100

    Missed some files

--- RP.Util.Exception/Class1.cs
+++ RP.Util.Exception/Class1.cs
@@ -3,38 +3,6 @@
 namespace RP.Util.Exception
 {
     /// <summary>
-    /// A static class of methods to wrap an operation in a try-catch and return the result of the operation.
-    /// </summary>
-    public static class Try
-    {
-        public static ExceptionableResult<T> To<T>(Func<T> work)
-        {
-            try
-            {
-                var result = work();
-                return new ExceptionableResult<T>(result);
-            }
-            catch (System.Exception exception)
-            {
-                return new ExceptionableResult<T>(exception);
-            }
-        }
-
-        public static ExceptionableResult To(Action work)
-        {
-            try
-            {
-                work();
-                return new ExceptionableResult();
-            }
-            catch (System.Exception exception)
-            {
-                return new ExceptionableResult(exception);
-            }
-        } 
-    }
-
-    /// <summary>
     /// The result of an operation wrapped in a <c>try</c> returning the exception on failure.
     /// </summary>
     public class ExceptionableResult
